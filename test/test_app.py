8af32f2bee980cc55c4d8eee9315b852ccce48b9
Harkanwal <harkanwal.sanghera@gmail.com>
2024-11-17 11:31:14 +0000

    Add test for creating a default app with create_app

--- test/test_app.py
+++ test/test_app.py
@@ -1,8 +1,19 @@
 from api.classes.spacex import SpaceX
+from api import create_app
 from flask import Flask
 
 
-def test_app_init(mock_app):
+def test_create_app_factory_testing(mock_app):
     assert isinstance(mock_app, Flask)
     assert mock_app.config["TESTING"]
     assert isinstance(mock_app.spacex_api, SpaceX)
+
+
+def test_create_app_factory_default(mocker, mock_spacex):
+    mocker.patch("api.dictConfig")
+    mock_spacex_class = mocker.patch("api.SpaceX")
+    mock_spacex_class.return_value = mock_spacex
+    app = create_app()
+
+    assert isinstance(app, Flask)
+    assert isinstance(app.spacex_api, SpaceX)
