13e96e960684f71c6f1867ec229fff3edc98acec
Harkanwal <harkanwal.sanghera@gmail.com>
2024-11-18 22:03:10 +0000

    Update styling

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,22 +1,33 @@
+import { Link } from 'react-router-dom'
+
 const Home = () => {
   return (
     <>
-      <div className="text-white w-[40vw] min-h-[70vh] items-center inline-flex justify-center">
-        <div className="flex-col justify-center items-center text-center gap-1.5 inline-flex">
+      <div className="text-white w-[50vw] min-h-[70vh] items-center inline-flex justify-center">
+        <div className="flex-col items-center">
+          <div className="flex-col justify-center items-center text-center gap-1 inline-flex">
             <div className="w-100 text-primary text-5xl font-bold uppercase ">SpaceX API Client</div>
-            <div className="text-center text-muted-foreground text-lg font-medium mt-8">
-              <p>
-                Welcome to the unofficial SpaceX API Client, which serves as a front-end for the Open Source 
-                REST API for launch and launch site data
-              </p>
-            </div>
-            <div>
-            <p className="italic text-center text-muted-foreground text-base font-medium mt-8">
-              This website or its associated Python Wrapper Module are not affiliated, associated, authorized, endorsed by, or in any way officially connected with Space 
-              Exploration Technologies Corp (SpaceX), or any of its subsidiaries or its affiliates. 
-              The names SpaceX as well as related names, marks, emblems and images are registered trademarks of their respective owners.
-            </p>
-            </div>
+                <div className="text-center text-muted-foreground text-lg mt-2">
+                  <p>
+                    Welcome to the unofficial SpaceX API Client, which serves as a front-end for the Open Source 
+                    REST API for launch and launch site data
+                  </p>
+                </div>
+          </div>
+          <div className="flex justify-center gap-5 p-5">
+            <Link to={'/launches'} className="box">
+              <div className="gap-2">
+                <h2 className="font-bold text-lg">Launches</h2>
+                <p>Explore the history of SpaceX launches, organized by year.</p>
+              </div>
+            </Link>
+            <Link to={'/launchsites'} className="box">
+              <div className="flex-col gap-2">
+                <h2 className="font-bold text-lg">Launch Sites</h2>
+                <p>Discover launch site history, and their launches.</p>
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </>
