7a127a42b425c5c263dd132d5efaa6fbf757cd5f
Harkanwal <harkanwal.sanghera@gmail.com>
2024-11-19 23:06:31 +0000

    Create a production config and include config type values for use in flask app

--- api/config.py
+++ api/config.py
@@ -1,6 +1,4 @@
 class Config:
-    TESTING = False
-    DEBUG = False
     LOG_LEVEL = "INFO"
     BASE_URL = "https://api.spacexdata.com"
 
@@ -8,14 +6,24 @@ class Config:
 class TestingConfig(Config):
     TESTING = True
     DEBUG = False
+    CONFIG_TYPE = "TEST"
 
 
 class DevConfig(Config):
     DEBUG = True
+    TESTING = False
+    CONFIG_TYPE = "DEV"
+
+
+class ProdConfig(Config):
+    TESTING = False
+    DEBUG = False
+    CONFIG_TYPE = "PROD"
 
 
 config = {
     "Testing": TestingConfig,
     "Default": DevConfig,
     "Development": DevConfig,
+    "Production": ProdConfig,
 }
