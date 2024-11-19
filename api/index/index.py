12274f630f0b3bf1d7216bd1d9a5f20cafb4fc13
Harkanwal <harkanwal.sanghera@gmail.com>
2024-11-19 23:07:19 +0000

    Serve the react app in production environment, otherwise treat API as standalone

--- api/index/index.py
+++ api/index/index.py
@@ -1,9 +1,13 @@
 from flask import Blueprint
-from flask import current_app as app
+from flask import current_app as app, jsonify
 
 index_bp = Blueprint("index", __name__)
 
 
 @index_bp.route("/")
 def index():
-    return app.send_static_file("index.html")
+    # Serve the react app from the root URL in production
+    if app.config["CONFIG_TYPE"] == "PROD":
+        return app.send_static_file("index.html")
+
+    return jsonify("Welcome to the SpaceX Python Wrapper API"), 200
