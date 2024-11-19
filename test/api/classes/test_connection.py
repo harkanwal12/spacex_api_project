179cb2930981fffb7a1bdc30dbc34c26866387c8
Harkanwal <harkanwal.sanghera@gmail.com>
2024-11-19 23:08:08 +0000

    Split out get and post unit tests

--- test/api/classes/test_connection.py
+++ test/api/classes/test_connection.py
@@ -33,23 +33,27 @@ def test_connection_get_success(mock_logger, mocker):
     mock_response = Mock()
     mock_response.status_code = 200
     mock_response.json.return_value = {"key": "value"}
-    requests_mock = mocker.patch("requests.get", return_value=mock_response)
+    requests_mock = mocker.patch(
+        "requests.request", return_value=mock_response
+    )
 
     conn = SpacexConnection("someurl/", False, mock_logger)
 
     response = conn.get("someendpoint")
 
-    requests_mock.assert_called_with("someurl/someendpoint", verify=False)
+    requests_mock.assert_called_with(
+        "GET", "someurl/someendpoint", verify=False, json=None
+    )
     assert response == {"key": "value"}
 
 
-def test_connection_get_404_error(mock_logger, mocker):
+def test_connection_get_error_no_data_found(mock_logger, mocker):
     mock_response = Mock()
     mock_response.status_code = 404
     mock_response.raise_for_status.side_effect = requests.exceptions.HTTPError(
         response=mock_response
     )
-    mocker.patch("requests.get", return_value=mock_response)
+    mocker.patch("requests.request", return_value=mock_response)
 
     conn = SpacexConnection("someurl/", False, mock_logger)
 
@@ -65,7 +69,7 @@ def test_connection_get_any_error(mock_logger, mocker):
     mock_response.raise_for_status.side_effect = requests.exceptions.HTTPError(
         response=mock_response
     )
-    mocker.patch("requests.get", return_value=mock_response)
+    mocker.patch("requests.request", return_value=mock_response)
 
     conn = SpacexConnection("someurl/", False, mock_logger)
 
@@ -79,7 +83,7 @@ def test_get_bad_json(mock_logger, mocker):
     mock_response.json.side_effect = ValueError(
         "No JSON object could be decoded"
     )
-    mocker.patch("requests.get", return_value=mock_response)
+    mocker.patch("requests.request", return_value=mock_response)
 
     conn = SpacexConnection("someurl/", False, mock_logger)
 
@@ -87,3 +91,55 @@ def test_get_bad_json(mock_logger, mocker):
         conn.get("someendpoint")
 
     assert str(err.value) == "Bad JSON in response"
+
+
+def test_connection_post_success(mock_logger, mocker):
+    query_response = {
+        "docs": [{"somekey2": "somevalue2"}, {"somekey1": "somevalue1"}],
+        "totalDocs": 2,
+        "offset": 0,
+        "limit": 2,
+        "totalPages": 1,
+        "page": 1,
+        "pagingCounter": 1,
+        "hasPrevPage": False,
+        "hasNextPage": False,
+        "prevPage": None,
+        "nextPage": None,
+    }
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = query_response
+    requests_mock = mocker.patch(
+        "requests.request", return_value=mock_response
+    )
+
+    conn = SpacexConnection("someurl/", False, mock_logger)
+
+    response = conn.post("someendpoint", {"somekey": "somevalue"})
+
+    requests_mock.assert_called_with(
+        "POST",
+        "someurl/someendpoint",
+        verify=False,
+        json={"somekey": "somevalue"},
+    )
+    assert response == [{"somekey2": "somevalue2"}, {"somekey1": "somevalue1"}]
+
+
+def test_connection_query_response_json_empty(mock_logger, mocker):
+    query_response = {
+        "docs": [],
+        "totalDocs": 0,
+    }
+    mock_response = Mock()
+    mock_response.status_code = 200
+    mock_response.json.return_value = query_response
+    mocker.patch("requests.request", return_value=mock_response)
+
+    conn = SpacexConnection("someurl/", False, mock_logger)
+
+    with pytest.raises(NoDataFoundException) as err:
+        conn.post("someendpoint", {"somekey": "somevalue"})
+
+    assert str(err.value) == "No data found for request"
